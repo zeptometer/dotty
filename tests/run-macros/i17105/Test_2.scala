5a8aa502cb3dcea547aac15a497a99da9d3dce75
Yuito Murase <yuito@acupof.coffee>
2023-06-04 22:15:29 +0900

    WIP: Add failing test case where method from outer scope got expanded accidentally

--- tests/run-macros/i17105/Test_2.scala
+++ tests/run-macros/i17105/Test_2.scala
@@ -1,3 +1,5 @@
 @main def Test: Unit =
   println("case single: " + testExpr { def f(x: String) = "placeholder" + x; f("1st") + " outside" })
   println("case curried: " + testExpr { def f(x: String)(y: String) = "placeholder" + x; f("1st")("2nd") + " outside" })
+  def outer() = "outside-method"
+  println("case methods from outer scope " + testExpr { def f(x: String) = "placeholder" + x; f("1st") + outer() })
