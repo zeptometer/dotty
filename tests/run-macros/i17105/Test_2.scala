74bef21da3f172132c2a3e1084556c60c3c2fb5b
Yuito Murase <yuito@acupof.coffee>
2023-06-04 15:01:10 +0900

    WIP: Add test case for curried method

--- tests/run-macros/i17105/Test_2.scala
+++ tests/run-macros/i17105/Test_2.scala
@@ -1,2 +1,3 @@
 @main def Test: Unit =
-  println(testExpr { def f(x: String) = "placeholder" + x; f("1st") + " 2nd" })
+  println("test single: " + testExpr { def f(x: String) = "placeholder" + x; f("arg1") + " outside" })
+  println("test curried: " + testExpr { def f(x: String)(y: String) = "placeholder" + x; f("arg1")("args2") + " outside" })
