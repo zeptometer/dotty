a93f1468800a2a59227f63091e5d6709c16990ee
Yuito Murase <yuito@acupof.coffee>
2023-06-07 22:41:01 +0900

    Add test case for refinement types

--- tests/run-macros/i17105/Macro_1.scala
+++ tests/run-macros/i17105/Macro_1.scala
@@ -7,17 +7,10 @@ def testExprImpl(body: Expr[Any])(using Quotes): Expr[String] =
       '{ $a((z: String) => s"[1st case] ${z}") }
     case '{ def g(y: String)(z: String) = "placeholder" + y; $a(g): String } =>
       '{ $a((z1: String) => (z2: String) =>  s"[2nd case] ${z1}, ${z2}") }
-    // case '{ def g[A](x: A) = List(); $a(g): String } =>
-    //   '{ $a([A] => (x: A) => List(x, x)) }
-    case _ => Expr("not matched")
-
-inline def testCtxParam(inline body: Any) = ${ testCtxParamImpl('body) }
-def testCtxParamImpl(body: Expr[Any])(using Quotes): Expr[String] =
-  body match
-    /* TODO issue-17105: Following cases causes type errors, which seems to be
-     * different issue from 17105 */
-    // case '{ given i: String = "given"; def g(using s: String) = "placeholder"; $a(g, i): String } =>
-    //   '{ $a((s: String) => s"[3rd case] ${s}")("another_given") }
-    // case '{ def g(using s: String) = "placeholder"; $a(g) } =>
-    //   '{ $a((s: String) => s"[3rd case] ${s}") }
+    case '{
+      type t
+      def p(a: `t`): String = $x(a): String
+      $y(p): String
+    } =>
+      '{ $y($x) }
     case _ => Expr("not matched")
--- tests/run-macros/i17105/Test_2.scala
+++ tests/run-macros/i17105/Test_2.scala
@@ -1,9 +1,11 @@
+import reflect.Selectable.reflectiveSelectable
+
+class Hoe { def f(x: Int): String = s"Hoe got ${x}" }
+
 @main def Test: Unit =
   println("case single: " + testExpr { def f(x: String) = "placeholder" + x; f("arg1") + " outside" })
   println("case no-param-method (will be eta-expanded): " + testExpr { def f(x: String) = "placeholder" + x; (() => f)()("placeholder 2") })
   println("case curried: " + testExpr { def f(x: String)(y: String) = "placeholder" + x; f("arg1")("arg2") + " outside" })
   def outer() = " outer-method"
   println("case methods from outer scope: " + testExpr { def f(x: String) = "placeholder" + x; f("arg1") + outer() })
-  // println("case polymorphic method: " + testExpr { def f[B](x: B) = List(); f[String]("arg").mkString(",") + " outside"})
-  // separate test case?
-  // println("case contextual params: " + testCtxParam { given String = "given"; def f(using t: String) = "placeholder"; f + " outside" })
+  println("case refinement: " + testExpr { def p(a: { def f(x: Int): String }): String = a.f(1); p(Hoe()) })
