ba174aea4d9a470875e64a1c1716faeace9f93da
Yuito Murase <yuito@acupof.coffee>
2023-08-07 11:22:26 +0900

    Add test case for neg-macros

--- tests/neg-macros/hoas-pattern-with-type-params.scala
+++ tests/neg-macros/hoas-pattern-with-type-params.scala
@@ -4,6 +4,8 @@ def test(body: Expr[Any])(using Quotes): Expr[String] =
   body match
     case '{ [A] => (x : A) => $b[A] : A => A } => // error // error
       Expr("Hoas pattern should always take value params")
+    case '{ [A] => (x : A) => $b(x) : A => A } => // error // error
+      Expr("`A` should be in type arguments because `x` depends on it")
     case '{ (a:Int) => $b[Int](a) : String } => // error
       Expr("Type params of a hoas pattern should be introduced inside the quote")
     case _ => Expr("not matched")
